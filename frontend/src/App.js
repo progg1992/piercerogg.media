ccb0cb662345d2a900c1f5f3312728d6db1b06fe
ptrogg1992 <ptrogg1992@gmail.com>
2022-09-22 18:00:03 -0400

    finished intial setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
+import { About, Footer, Header, Skills, Testimonials, Projects } from './container';
+
 const App = () => {
     return (
-        <h1>App</h1>
-    )
-}
+        <div className='app'>
+            <Header />
+            <About />
+            <Projects />
+            <Skills />
+            <Testimonials />
+            <Footer />
+        </div>
+    );
+};
 
 export default App;

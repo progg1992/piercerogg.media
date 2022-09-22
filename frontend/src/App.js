6a9e706c2817057bc558e0a1a7368732be3ee76e
ptrogg1992 <ptrogg1992@gmail.com>
2022-09-22 18:08:20 -0400

    added basic styles and code for Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 import { About, Footer, Header, Skills, Testimonials, Projects } from './container';
+import { Navbar } from './component/';
+import './App.scss';
 
 const App = () => {
     return (
         <div className='app'>
+            <Navbar />
             <Header />
             <About />
             <Projects />
